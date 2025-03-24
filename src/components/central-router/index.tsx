78968e44f94f0282f7bf7bd78ea73e558856c25f
lenconda <i@lenconda.top>
2025-03-24 19:19:31 +0800

    fix: 🐛 central-router issues

--- src/components/central-router/index.tsx
+++ src/components/central-router/index.tsx
@@ -1,6 +1,7 @@
-import { StringUtil } from '@open-norantec/utilities';
+import { StringUtil } from '@open-norantec/utilities/dist/string-util.class';
 import * as React from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
+import * as _ from 'lodash';
 
 export interface Page {
     layoutPath?: string;
@@ -9,12 +10,13 @@ export interface Page {
 }
 
 export interface CentralRouterProps<T> {
-    router?: React.FC<T>;
+    router?: (props: T) => React.JSX.Element;
     routerProps?: T;
     page?: Page;
 }
 
 const generateRoutes = (page: Page, pathname: string) => {
+    if (!_.isPlainObject(page)) return null;
     return (
         <Route
             path={StringUtil.isFalsyString(pathname) ? '/' : pathname}
